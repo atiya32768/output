d206d190d8dc1574b64d495f073cbd5a8ee5784a
atiya32768 <atiya.mahboob@novanta.com>
2022-08-01 14:31:11 +0100

    update output.py
    Used the zip function

--- output.py
+++ output.py
@@ -1,4 +1,3 @@
-
 import contextlib  
 import numpy as np
 from adafruit_macropad import MacroPad
@@ -9,7 +8,8 @@ from adafruit_hid.keycode import Keycode
 
 my_lines=[]
 input_list=[]
-num_of_output=0
+num_of_output=0 # a internal counter
+output_file_path="E:\output"
 file_path="C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Laser Quantum Ltd\LQ Terminal.lnk"
 with open(file_path, "w") as text_file:
     with contextlib.redirect_stdout(text_file):
@@ -17,18 +17,32 @@ with open(file_path, "w") as text_file:
             num_of_output +=1
             my_lines.append(my_lines)
         print("The output is added to the list in the file")
-        splits=np.split(my_lines, num_of_output)
+        # splits=np.split(my_lines, num_of_output)
 # we need a way to assign the specific commands with the numbers
 # mylines[0][num_of_output]
 # length of the array is the num_of_output
-#create a function instead
 
         for element in range(0, num_of_output):
             while True:
                 key_event = macropad.keys.events.get()
-                if key_event and key_event.pressed and key_event !='N/A':
+                if key_event and key_event.pressed and key_event !='':
                     continue
-                if key_event !='N/A' and 
+                if key_event.key_number =='ENTER' and key_event.pressed:
+                    continue
+                if key_event.key_number != '' and key_event.key_number != 'ENTER':
+                    input_list.append(key_event.key_number)
+
+
+# we compare the lists to each other using the same position
+        for i, j, k in zip(my_lines, input_list):
+            output_file_path="E:\output"
+            x=zip(input_list,my_lines)
+            tuple(x)
+
+
+            # element at that position 
+
+                
                 
 
                     
