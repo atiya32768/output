f7db1ddc6b90fbde54018df9746451304139d221
atiya32768 <atiya.mahboob@novanta.com>
2022-08-02 12:43:18 +0100

    Update output.py
    Edited the first for loop

--- output.py
+++ output.py
@@ -17,30 +17,32 @@ while True:
     key_event = macropad.keys.events.get()
     with open(file_path, "a") as text_file:
         with contextlib.redirect_stdout(text_file):
-            for my_lines in text_file:
+            for my_line in text_file:
                # print out output at that specific position
                 num_of_output +=1
                 if key_event.key_number==1 and key_event.pressed and macropad.encoder_switch==True: # prints out the output at that specific moment 
                     one_output=macropad.display_text(my_lines[num_of_output])
                     one_output.show() 
                 else:
-                    if key_event.key_number==2 and key_event.pressed and macropad.encoder_switch==True:
+                    if key_event and key_event.pressed and key_event.key_number==2 and macropad.encoder_switch==True:
                         continue
                 # display each output in turn onto the macropad screen for a specific amount of time
-                my_lines.append(my_lines)
+                my_lines.append(my_line)
             logging.debug("The output is added to a list in the file")
             # splits=np.split(my_lines, num_of_output)
     # we need a way to assign the specific commands with the numbers
     # length of the array is the num_of_output
 
             for element in range(0, num_of_output):
-                if key_event and key_event.pressed and key_event !='':
-                    continue
-                if key_event.key_number ==11 and key_event.pressed:
-                    continue
-                if key_event.key_number != '' and key_event.key_number != 'ENTER': #apppend the initial commands into a list
-                    input_list.append(key_event.key_number)
-
+                while True:
+                    key_event and key_event.pressed
+                    if key_event != '' and key_event != 'ENTER': 
+                        input_list.append(key_event)
+                    if key_event.pressed and key_event !='':    # they could press an equals button/or nothing
+                        continue
+                    if key_event.key_number == 11 and key_event.pressed:
+                        continue
+                    #apppend the initial commands into a list
     # we compare the lists to each other using the same position
             for i, j, k in list(zip(my_lines, input_list)):
                 output_file_path="E:\output"
