2dac7a812ab742bf77a6e067f632a8372cd02264
atiya32768 <atiya.mahboob@novanta.com>
2022-08-02 14:35:12 +0100

    update send_message.py
    The file was updated with an if statement.

--- send_message.py
+++ send_message.py
@@ -2,11 +2,20 @@
 
 from adafruit_macropad import MacroPad
 macropad = MacroPad()
-import logging
 file_path="C:\Users\atiya.mahboob\OneDrive - Novanta\Documents\test_file.txt"
 number=[]
 letter=[]
 while True:
     key_event = macropad.keys.events.get()
     if key_event.key_number==3 and key_event.pressed and macropad.encoder_switch==True:
-        
+         for i, j  in list(zip(number, letter)):
+                output_file_path="E:\output"
+                x=list(zip(number, letter))  # use zip function to format lists in a specific way
+                tuple(x) 
+                tuple_no_space=str(tuple(x)).replace(', ', '')
+
+    text_lines= macropad.display_text(title="Commands & Output")
+    if key_event.key_number==2 and key_event.pressed and macropad.encoder_switch==True:
+        for i in range(len(tuple_no_space)):
+            text_lines[i].text=macropad.display_text(tuple_no_space[i])
+            text_lines.show()
